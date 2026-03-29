f90f2fd0dee68a3de2095f9b868ee272d831cff2
can4to-lab <can4to@gmail.com>
2026-03-29 13:10:49 +0300

    parakende satış 3

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm'; // 👈 EKLENDİ
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { Product } from './entities/product.entity'; // 👈 EKLENDİ
-
+import { Category } from './entities/category.entity';
+import { ProductVariant } from './entities/product-variant.entity';
 @Module({
   imports: [
     // 👇 BU SATIR EKSİKTİ, O YÜZDEN HATA VERİYORDU
-    TypeOrmModule.forFeature([Product]), 
+    TypeOrmModule.forFeature([Product, Category, ProductVariant]), 
   ],
   controllers: [ProductsController],
   providers: [ProductsService],
